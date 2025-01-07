ff63d10f8304a003efb297cf043e0eab6dbc714e
thsvo <155262940+thsvo@users.noreply.github.com>
2025-01-07 23:50:57 +0600

    finish

---
+++ src/utilities/configs/base_api.js
@@ -0,0 +1,6 @@
+export const base_url = "https://abheen-server-new.vercel.app/api/v1";
+export const base_url_image = "https://abheen-server-new.vercel.app/";
+// export const base_url = "http://localhost:5000/api/v1";
+// export const base_url_image = "http://localhost:5000/";
+// export const base_url = "http://192.168.1.6:5000/api/v1";
+// export const base_url_image = "http://192.168.1.6:5000/";
